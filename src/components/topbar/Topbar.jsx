109a0a6c9f1d6809301961c3eda871eb6318634c
Cansu Uçankale <54913401+ucankalecansu@users.noreply.github.com>
2022-02-17 21:44:50 +0300

    updates the required files

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+import "./topbar.css";
+
+export default function Topbar() {
+  const user = true;
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fab fa-facebook-square"/>
+        <i className="topIcon fab fa-instagram-square"/>
+          <i className="topIcon fab fa-github-square"/>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">Anasayfa</Link>
+          </li>
+          <li className="topListItem">Hakkımda</li>
+          <li className="topListItem">İletişim</li>
+          <li className="topListItem"><Link className="link" to="/write">Yazı Yaz</Link></li>
+          {user && <li className="topListItem">Çıkış Yap</li>}
+        </ul>
+      </div>
+      <div className="topRight">
+        {user ? (
+          <Link className="link" to="/settings">
+            <img
+              className="topImg"
+              src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+            />
+          </Link>
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                Giriş Yap
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                Kayıt Ol
+              </Link>
+            </li>
+          </ul>
+        )}
+        <i className="topSearchIcon fas fa-search"/>
+      </div>
+    </div>
+  );
+}
